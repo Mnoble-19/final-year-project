03dcc9fd3d6aae38c533afbad0e6e42f15be0248
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2023-06-16 16:29:48 +0300

    validation and cleanup

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -3,7 +3,6 @@ import 'package:mobile/constants/constants.dart';
 import 'package:mobile/screens/home.dart';
 import 'package:mobile/screens/signup_screen.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -14,6 +13,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,179 +22,208 @@ class _LoginScreenState extends State<LoginScreen> {
       extendBodyBehindAppBar: true,
       body: Padding(
         padding: EdgeInsets.fromLTRB(16, 0, 17, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 359,
-              height: 265,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-                borderRadius: BorderRadius.circular(0),
-                color: CustomColors.appDarkGreenColor,
-              ),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(17, 0, 0, 18),
-                child: Container(
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 359,
+                  height: 265,
                   decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                     borderRadius: BorderRadius.circular(0),
                     color: CustomColors.appDarkGreenColor,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Welcome to",
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Colors.white.withOpacity(0.8),
-                            ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(17, 0, 0, 18),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(0),
+                        color: CustomColors.appDarkGreenColor,
                       ),
-                      RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'E',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 60,
-                                  ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Welcome to",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Colors.white.withOpacity(0.8),
+                                ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'E',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(
+                                        color: Colors.white.withOpacity(0.8),
+                                        fontSize: 60,
+                                      ),
+                                ),
+                                TextSpan(
+                                  text: '-tolls',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(
+                                        fontSize: 60,
+                                      ),
+                                ),
+                              ],
                             ),
-                            TextSpan(
-                              text: '-tolls',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    fontSize: 60,
-                                  ),
-                            ),
-                          ],
-                        ),
+                          ),
+                          // const SizedBox(height: 16.0),
+                          Text(
+                            "Drive smart, go digital!",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontWeight: FontWeight.w400,
+                                ),
+                          ),
+                          const SizedBox(height: 16.0),
+                        ],
                       ),
-                      // const SizedBox(height: 16.0),
-                      Text(
-                        "Drive smart, go digital!",
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Colors.white.withOpacity(0.8),
-                              fontWeight: FontWeight.w400,
-                            ),
-                      ),
-                      const SizedBox(height: 16.0),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 41.0),
-            const Align(
-              heightFactor: 0.5,
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Log in',
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Email Address',
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-              ),
-            ),
-            const SizedBox(height: 19.0),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: CustomColors.appTransluscentGreenColor,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Password',
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-              ),
-            ),
-            const SizedBox(height: 19.0),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: CustomColors.appTransluscentGreenColor,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  CustomColors.appDarkGreenColor,
-                ),
-              ),
-              onPressed: () {
-                // TODO: Implement login logic
-                
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const MyHomePage(),
-                  ),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(91, 18, 91, 17),
+                const SizedBox(height: 41.0),
+                const Align(
+                  heightFactor: 0.5,
+                  alignment: Alignment.topLeft,
                   child: Text(
-                    'Login',
+                    'Log in',
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Email Address',
                     style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          fontSize: 20,
-                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
                         ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 11.0),
-            Flexible(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SignUpScreen(),
-                    ),
-                  );
-                },
-                child: Text(
-                  'Sign up',
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        fontWeight: FontWeight.w700,
-                      ),
+                const SizedBox(height: 19.0),
+                TextFormField(
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an email address';
+                    }
+                    final emailRegex =
+                        RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+                    if (!emailRegex.hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: CustomColors.appTransluscentGreenColor,
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-            ),
-          ],
-        ),
+                const SizedBox(height: 16.0),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Password',
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontWeight: FontWeight.w500,
+                        ),
+                  ),
+                ),
+                const SizedBox(height: 19.0),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a Password!';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: CustomColors.appTransluscentGreenColor,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      CustomColors.appDarkGreenColor,
+                    ),
+                  ),
+                  onPressed: () {
+                    
+                    if (_formKey.currentState!.validate()) {
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
+                      // login(email, password);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MyHomePage(),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(91, 18, 91, 17),
+                      child: Text(
+                        'Login',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 11.0),
+                Flexible(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SignUpScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Sign up',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w700,
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
