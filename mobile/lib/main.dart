7e022253d6f1b1dc11ad9cd87db25d9e9a0eb7c2
Emma Bryan Wandera <68502967+BryanWandera@users.noreply.github.com>
2023-07-18 17:58:51 +0300

    updated initial route

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
 
-        home: const MyHomePage(),
+        home: const LoginScreen(),
 
       ),
     );
